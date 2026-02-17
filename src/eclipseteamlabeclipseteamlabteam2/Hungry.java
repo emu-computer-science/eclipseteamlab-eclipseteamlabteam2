46416f3c1150fa6f5499af6b961e0b009b9508ab
Ahmad Atris <YOUR_GITHUB_EMAIL>
2026-02-17 16:44:44 -0500

    Add hungry2 method and update Driver

--- src/eclipseteamlabeclipseteamlabteam2/Hungry.java
+++ src/eclipseteamlabeclipseteamlabteam2/Hungry.java
@@ -5,4 +5,7 @@ public class Hungry {
     public static void hungry() {
         System.out.println("We are feeling hungry.");
     }
+    public static void hungry2() {
+    	System.out.println("Still hungry... someone order food.");
+    }
 }
